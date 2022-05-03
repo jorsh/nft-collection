c554ef748a53f4e63af98e167be883607458258b
Jorge Arenas <jorshx@gmail.com>
2022-05-02 20:41:38 -0500

    Updated structure for OpenSea

--- webapp/pages/api/[tokenId].js
+++ webapp/pages/api/[tokenId].js
@@ -1,16 +1,11 @@
 export default function handler(req, res) {
-    // get the tokenId from the query params
     const tokenId = req.query.tokenId;
-    // As all the images are uploaded on github, we can extract the images from github directly.
-    const image_url =
-        "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
-    // The api is sending back metadata for a Crypto Dev
-    // To make our collection compatible with Opensea, we need to follow some Metadata standards
-    // when sending back the response from the api
-    // More info can be found here: https://docs.opensea.io/docs/metadata-standards
+    const image_url = "https://raw.githubusercontent.com/jorsh/nft-collection/main/webapp/public/cryptodevs/";
+
     res.status(200).json({
-        name: "Crypto Dev #" + tokenId,
-        description: "Crypto Dev is a collection of developers in crypto",
+        name: "Crypto Badge #" + tokenId,
+        description: "Crypto Badges is a collection of crypto assets",
         image: image_url + tokenId + ".svg",
+        external_url: "https://github.com/jorsh/nft-collection/tree/main/webapp/public/cryptodevs"
     });
 }
